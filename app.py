61c0d0ca62f96470fb80fbebb27234a9d94f3213
alef-asilva <alef.araujo@outlook.com.br>
2024-02-06 21:48:18 -0300

    Alef feito as rotas e controller tambem

--- app.py
+++ app.py
@@ -42,4 +42,20 @@ app.route("/sectors/<int:id>", methods = ["GET"])(get_sectors_by_id)
 app.route("/sectors/<int:id>", methods=["PUT"])(put_sectors)
 
 
+from routes.route_administrators import bp_administrators
+from routes.route_neighborhoods import bp_neighborhoods
+from routes.route_night_guards import bp_night_guards
+from routes.route_sectors import bp_sectors
+
+app = Flask(__name__)
+
+app.register_blueprint(bp_administrators)
+app.register_blueprint(bp_neighborhoods)
+app.register_blueprint(bp_night_guards)
+app.register_blueprint(bp_sectors)
+
+
+
+
+
 app.run(debug=True)
