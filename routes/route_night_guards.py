a1773e0ca1a3c556fb9d8cdf721d40a74014b8fb
alef-asilva <alef.araujo@outlook.com.br>
2024-04-15 22:07:23 -0300

    Alef(Concertando erro na models)

--- routes/route_night_guards.py
+++ routes/route_night_guards.py
@@ -1,14 +1,13 @@
 from flask import Blueprint
-from controllers.controller_nightguards import put_night_guards, get_nightguards, post_nightguards, delete_night_guards_by_id,get_night_guards_by_id
+from controllers.controller_nightguards import put_night_guards, get_nightguards, delete_night_guards_by_id,get_night_guards_by_id
 
 bp_night_guards = Blueprint ("bp_night_guards",__name__)
 
 
-bp_night_guards.route("/post_nightguard", methods=["POST"])(post_nightguards)
 
 bp_night_guards.route("/get_nightguard", methods=["GET"])(get_nightguards)
 
-#bp_night_guards.route("/night_guards/<int:id>", methods = ["GET"])(get_night_guards_by_id)
+bp_night_guards.route("/night_guards/<int:id>", methods = ["GET"])(get_night_guards_by_id)
 
 bp_night_guards.route("/night_guards/<int:id>",methods=["PUT"])(put_night_guards)
 
